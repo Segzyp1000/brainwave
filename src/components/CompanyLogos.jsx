8e3fe0c74181c934622e1d24a2cec339f562ac32
segun olowoyeye <segunolowoyeye@gmail.com>
2024-10-27 06:35:07 +0100

    third commit

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -1,4 +1,4 @@
-import { companyLogos } from "../constants";
+import { companyLogos } from "../constants/origin";
 
 const CompanyLogos = ({ className }) => {
   return (
--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -1,5 +1,5 @@
 import { check } from "../assets";
-import { pricing } from "../constants";
+import { pricing } from "../constants/origin";
 import Button from "./Button";
 
 const PricingList = () => {
